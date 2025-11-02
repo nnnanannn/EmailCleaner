0cf1d83bd7780b24cd97b21fb25d47da920ae1c8
nnnanannn <yuttitham_ning@hotmail.com>
2025-11-02 09:08:31 +0900

    feat: Add Javadoc comments,  Remove setters, Add helper methods
    - Remove setters because emails shouldn't change after creation
    - Add to CompactString for shorten representation string of emails

--- src/test/EmailTest.java
+++ src/test/EmailTest.java
@@ -2,6 +2,14 @@ package test;
 
 public class EmailTest {
 
+    /**
+     * id - Unique identifier for each email
+     * from - Sender's email address
+     * to - Recipient's email address
+     * subject - Email subject line
+     * date - Date email was sent
+     * body - Email content/ message
+     */
     private int id; // Unique identifier for each email
     private String from;
     private String to;
@@ -9,6 +17,15 @@ public class EmailTest {
     private String date;
     private String body;
 
+    /**
+     * Field
+     * @param id - Unique identifier
+     * @param from - Sender email address
+     * @param to - Recipient email address
+     * @param subject - Email subject
+     * @param date - Date sent
+     * @param body - Email content
+     */
     public EmailTest(int id, String from, String to, String subject, String date, String body) {
         this.id = id;
         this.from = from;
@@ -17,63 +34,89 @@ public class EmailTest {
         this.body = body;
     }
 
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
+    /**
+     * Get the email's unique ID
+     * @return The email ID
+     */
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    /**
+     * Get the sender's email address
+     * @return The from email address
+     */
+    public String getFrom() {
+        return from;
     }
 
-    @Override  // This overrides the default toString() method from Object class
+    /**
+     * Get the sender's email address
+     * @return The to email address
+     */
+    public String getTo() {
+        return to;
+    }
+
+    /**
+     * Get the email subject
+     * @return The subject lin
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * Get the date the email was sent
+     * @return The date string
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * Get the full email body
+     * @return The complete email content
+     */
+    public String getBody() {
+        return body;
+    }
+
+    /**
+     * @return Formatted string representation of the email
+     */
+    @Override
     public String toString() {
         return String.format(
-                "ID: %d\nFrom: %s\nTo: %s\nSubject: %s\nDate: %s\nBody: %s\n",
-                id, from, to, subject, date, body
+                "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━%n" +
+                        "ID:      %d%n" +
+                        "From:    %s%n" +
+                        "To:      %s%n" +
+                        "Subject: %s%n" +
+                        "Date:    %s%n" +
+                        "Body:    %s%n" +
+                        "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
+                id, from, to, subject, date, getBodyPreview()
         );
     }
 
+    /**
+     * @return First 100 characters of body, or full body if shorter
+     * If body is longer than 100 chars, take first 100 and add "..."
+     * Otherwise, return the full body
+     */
     public String getBodyPreview() {
         return body.length() > 100 ? body.substring(0, 100) + "..." : body;
+    }
+
+    /**
+     * Alternative: Compact display format for list views
+     * Shows just essential info in one line
+     *
+     * @return One-line summary of the email
+     */
+    public String toCompactString() {
+        return String.format("[%d] %s | %s | %s",
+                id, date, from, subject);
     }
 }
